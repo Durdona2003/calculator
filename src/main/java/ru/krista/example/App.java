c6a0c4c0e40f88994a6051ef6554fad3de6a3c49
Вадим <sokolov_vadim95@bk.ru>
2024-02-29 12:02:22 +0300

    Initial commit

---
+++ src/main/java/ru/krista/example/App.java
@@ -0,0 +1,63 @@
+package ru.krista.example;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ */
+public final class App {
+    private App() {
+    }
+
+    /**
+     * Says hello to the world.
+     * 
+     * @param args The arguments of the program.
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите выражение: ");
+        String str = in.nextLine();
+        in.close();
+
+        String[] parts = str.split(" ");
+        int num1;
+        int num2;
+        try {
+            // получение числа из строки
+            num1 = Integer.parseInt(parts[0]);
+            num2 = Integer.parseInt(parts[2]);
+        } catch (NumberFormatException e) {
+            System.out.println("Не удалось получить число");
+            return;
+        }
+
+        String operation = parts[1];
+        Integer result = 0;
+        switch (operation) {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+
+                if (num2 == 0) {
+                    System.out.println("Деление на ноль нельзя");
+                    return;
+                } else {
+                    result = num1 / num2;
+                    break;
+                }
+            default:
+                System.out.println("Операция не распознана");
+                return;
+        }
+        System.out.println("Результат: " + result.toString());
+    }
+
+}
